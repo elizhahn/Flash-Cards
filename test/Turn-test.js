340f0db5a62f525afbcc85cd66de30d43b2e063b
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-01 19:07:54 -0700

    Write tests for returnCard and returnGuess methods

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -14,4 +14,22 @@ describe("Turn", function () {
     expect(turn).to.be.an.instanceof(Turn);
   });
 
+  it("should return a user's guess", function () {
+    const turn = new Turn("object");
+    const userGuess = turn.returnGuess();
+    expect(userGuess).to.equal("object");
+  });
 
+  it("should return the current card", function () {
+    const card = new Card(
+      1,
+      "What allows you to define a set of related information using key-value pairs?",
+      ["object", "array", "function"],
+      "object"
+    );
+    const turn = new Turn("object", card);
+    const currentCard = turn.returnCard();
+    expect(currentCard).to.be.an.instanceof(Card);
+    expect(currentCard).to.deep.equal(card);
+  });
+});
