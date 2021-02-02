55db252048ecb3a19a065c9c4a70ccdb4f06655d
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-01 22:11:03 -0700

    Create test for countCards method

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -38,4 +38,31 @@ describe("Deck", function () {
     expect(deck.cards[0]).to.deep.equal(card1);
     expect(deck.cards.length).to.equal(2);
   });
+
+  it("should know how many cards are in the deck", function () {
+    const card1 = new Card(
+      1,
+      "What allows you to define a set of related information using key-value pairs?",
+      ["object", "array", "function"],
+      "object"
+    );
+    const card2 = new Card(
+      2,
+      "What is a comma-separated list of related values?",
+      ["array", "object", "function"],
+      "array"
+    );
+    const card3 = new Card(
+      3,
+      "What type of prototype method directly modifies the existing array?",
+      ["mutator method", "accessor method", "iteration method"],
+      "mutator method"
+    );
+
+    const deck = new Deck([card1, card2, card3]);
+
+    const numCards = deck.countCards();
+
+    expect(numCards).to.equal(2);
+  });
 });
