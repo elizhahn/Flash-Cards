109cba4522c6f212d5c1d7b8e3eaaa1aed455f60
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-01 21:09:15 -0700

    Create evaluateGuess and giveFeedback methods and pass tests

--- src/Turn.js
+++ src/Turn.js
@@ -2,12 +2,30 @@ class Turn {
   constructor(guess, card) {
     this.guess = guess;
     this.card = card;
+    this.isCorrect = true;
   }
+
   returnGuess() {
-    return this.userGuess;
+    return this.guess;
   }
+
   returnCard() {
     return this.card;
+  }
+
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) {
+      this.isCorrect = true;
+    } else {
+      this.isCorrect = false;
+    }
+  }
+  giveFeedback() {
+    if (this.isCorrect) {
+      return "correct!";
+    } else {
+      return "incorrect!";
+    }
   }
 }
 
