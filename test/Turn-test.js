372a77c89979d0fbda56afbbbac57c45960fed90
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-01 19:25:48 -0700

    Write test for giveFeedback method

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -32,4 +32,50 @@ describe("Turn", function () {
     expect(currentCard).to.be.an.instanceof(Card);
     expect(currentCard).to.deep.equal(card);
   });
+
+  it("should evaluate if the guess was correct or incorrect", function () {
+    const card1 = new Card(
+      1,
+      "What allows you to define a set of related information using key-value pairs?",
+      ["object", "array", "function"],
+      "object"
+    );
+    const card2 = new Card(
+      2,
+      "What is a comma-separated list of related values?",
+      ["array", "object", "function"],
+      "array"
+    );
+    const turn1 = new Turn("object", card1);
+    const turn2 = new Turn("object", card2);
+
+    turn1.evaluateGuess();
+    turn2.evaluateGuess();
+
+    expect(turn1.isCorrect).to.equal(true);
+    expect(turn2.isCorrect).to.equal(false);
+  });
+
+  it("should provide feedback based on the guess", function () {
+    const card1 = new Card(
+      1,
+      "What allows you to define a set of related information using key-value pairs?",
+      ["object", "array", "function"],
+      "object"
+    );
+    const card2 = new Card(
+      2,
+      "What is a comma-separated list of related values?",
+      ["array", "object", "function"],
+      "array"
+    );
+    const turn1 = new Turn("object", card1);
+    const turn2 = new Turn("function", card2);
+
+    const feedback1 = turn1.giveFeedBack();
+    const feedback2 = turn2.giveFeedBack();
+
+    expect(feedback1).to.equal("correct!");
+    expect(feedback2).to.equal("incorrect!");
+  });
 });
