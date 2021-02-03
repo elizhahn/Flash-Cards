7aff7a8c37df00f4962efdd53ed63b3ee72be5dd
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-03 16:27:42 -0700

    Use beforeEach hook to DRY up tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,54 +4,55 @@ const Card = require("../src/Card");
 const Turn = require("../src/Turn");
 
 describe("Turn", function () {
-  it("should be a function", function () {
-    expect(Turn).to.be.a("function");
-  });
-
-  it("should be an instance of Turn", function () {
-    const turn = new Turn();
-    expect(turn).to.be.an.instanceof(Turn);
-  });
-
-  it("should return a user's guess", function () {
-    const turn = new Turn("object");
-    const userGuess = turn.returnGuess();
-
-    expect(turn.guess).to.equal("object");
-    expect(userGuess).to.equal("object");
-  });
-
-  it("should return the current card", function () {
-    const card = new Card(
+  let card1;
+  let card2;
+  let turn1;
+  let turn2;
+  beforeEach("instantiate Cards and a Turn", function () {
+    card1 = new Card(
       1,
       "What allows you to define a set of related information using key-value pairs?",
       ["object", "array", "function"],
       "object"
     );
-    const turn = new Turn("object", card);
-    const currentCard = turn.returnCard();
-
-    expect(turn.card).to.equal(card);
-    expect(currentCard).to.be.an.instanceof(Card);
-    expect(currentCard).to.deep.equal(card);
-  });
-
-  it("should evaluate if the guess was correct or incorrect", function () {
-    const card1 = new Card(
-      1,
-      "What allows you to define a set of related information using key-value pairs?",
-      ["object", "array", "function"],
-      "object"
-    );
-    const card2 = new Card(
+    card2 = new Card(
       2,
       "What is a comma-separated list of related values?",
       ["array", "object", "function"],
       "array"
     );
-    const turn1 = new Turn("object", card1);
-    const turn2 = new Turn("object", card2);
+    turn1 = new Turn("object", card1);
+    turn2 = new Turn("object", card2);
+  });
+  it("should be a function", function () {
+    expect(Turn).to.be.a("function");
+  });
 
+  it("should be an instance of Turn", function () {
+    expect(turn1).to.be.an.instanceof(Turn);
+  });
+
+  it("should return a user's guess", function () {
+    const userGuess = turn1.returnGuess();
+
+    expect(turn1.guess).to.equal("object");
+    expect(userGuess).to.equal("object");
+  });
+
+  it("should return the current card", function () {
+    const currentCard1 = turn1.returnCard();
+    const currentCard2 = turn2.returnCard();
+
+    expect(turn1.card).to.equal(card1);
+    expect(currentCard1).to.be.an.instanceof(Card);
+    expect(currentCard1).to.deep.equal(card1);
+
+    expect(turn2.card).to.equal(card2);
+    expect(currentCard2).to.be.an.instanceof(Card);
+    expect(currentCard2).to.deep.equal(card2);
+  });
+
+  it("should evaluate if the guess was correct or incorrect", function () {
     const guess1 = turn1.evaluateGuess();
     const guess2 = turn2.evaluateGuess();
 
@@ -60,21 +61,6 @@ describe("Turn", function () {
   });
 
   it("should provide feedback based on the guess", function () {
-    const card1 = new Card(
-      1,
-      "What allows you to define a set of related information using key-value pairs?",
-      ["object", "array", "function"],
-      "object"
-    );
-    const card2 = new Card(
-      2,
-      "What is a comma-separated list of related values?",
-      ["array", "object", "function"],
-      "array"
-    );
-    const turn1 = new Turn("object", card1);
-    const turn2 = new Turn("function", card2);
-
     turn1.evaluateGuess();
     turn2.evaluateGuess();
 
