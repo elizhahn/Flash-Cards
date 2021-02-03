4eb996c8d8fe1ae78f0c3aecfec2ca7c5109ce50
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-03 11:50:29 -0700

    Fix start method to use prototype data variable

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,35 @@
-const data = require('./data');
+const data = require("./data");
 const prototypeQuestions = data.prototypeData;
-const util = require('./util');
+const util = require("./util");
+const Round = require("../src/Round.js");
+const Deck = require("../src/Deck.js");
+const Card = require("../src/Card.js");
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = {};
+  }
+  start() {
+    const cards = [];
+    prototypeQuestions.forEach((card) => {
+      cards.push(
+        new Card(card.id, card.question, card.answers, card.correctAnswer)
+      );
+    });
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+    this.currentRound = round;
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
 
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
+    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+-----------------------------------------------------------------------`);
   }
 
   printQuestion(round) {
-      util.main(round);
+    util.main(round);
   }
 }
 
