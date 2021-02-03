7d5939ec048a43f9e790a5d0fc2aaf8172a4810d
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-02 23:03:33 -0700

    Write start method tests for game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -4,7 +4,6 @@ const Game = require("../src/Game.js");
 const Round = require("../src/Round.js");
 const Deck = require("../src/Deck.js");
 const Card = require("../src/Card.js");
-const Turn = require("../src/Turn.js");
 
 describe("Game", function () {
   it("should be a function", function () {
@@ -13,26 +12,64 @@ describe("Game", function () {
 
   it("should instantiate a new Game", function () {
     const game = new Game();
+
     expect(game).to.be.aninstanceof(Game);
   });
 
-  it("should keep track of the current round", function () {
-    const card1 = new Card(
-      1,
-      "What allows you to define a set of related information using key-value pairs?",
-      ["object", "array", "function"],
-      "object"
-    );
-    const card2 = new Card(
-      2,
-      "What is a comma-separated list of related values?",
-      ["array", "object", "function"],
-      "array"
-    );
-    const deck = new Deck([card1, card2]);
-    const round = new Round(deck);
-    const game = new Game(round);
+  it("should start off with a default currentRound", function () {
+    const game = new Game();
 
-    expect(game.currentRound).to.deep.equal(game);
+    expect(game.currentRound).to.deep.equal({});
+  });
+
+  describe("start the game", function () {
+    it("should create cards for a round and put them in a deck", function () {
+      const data = [
+        {
+          id: 1,
+          question:
+            "What allows you to define a set of related information using key-value pairs?",
+          answers: ["object", "array", "function"],
+          correctAnswer: "object",
+        },
+        {
+          id: 2,
+          question: "What is a comma-separated list of related values?",
+          answers: ["array", "object", "function"],
+          correctAnswer: "array",
+        },
+      ];
+      const game = new Game();
+      const deck = game.start();
+
+      expect(deck[0]).to.be.aninstanceof(Card);
+      expect(deck[1]).to.be.aninstanceof(Card);
+      expect(deck[0]).to.deep.equal(data[0]);
+      expect(deck[1]).to.deep.equal(data[1]);
+    });
+
+    it("should create a new round", function () {
+      const data = [
+        {
+          id: 1,
+          question:
+            "What allows you to define a set of related information using key-value pairs?",
+          answers: ["object", "array", "function"],
+          correctAnswer: "object",
+        },
+        {
+          id: 2,
+          question: "What is a comma-separated list of related values?",
+          answers: ["array", "object", "function"],
+          correctAnswer: "array",
+        },
+      ];
+      const game = new Game();
+      game.start();
+
+      expect(game.currentRound).to.be.aninstanceof(Round);
+      expect(game.currentRound.deck).to.deep.equal(data);
+      expect(game.currentRound.deck[0]).to.be.aninstanceof(Card);
+    });
   });
 });
