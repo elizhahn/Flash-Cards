73da9f1dccace56e9faf3d83c26ffff6609af75b
Elizabeth Hahn <elizhahn87@gmail.com>
2021-02-02 20:12:00 -0700

    Create returnCurrentCard method and pass test

--- src/Round.js
+++ src/Round.js
@@ -1,8 +1,16 @@
+const Turn = require("./Turn.js");
+
 class Round {
   constructor(deck) {
+    this.deck = deck.cards;
     this.currentCard = deck.cards[0];
     this.turnCount = 0;
     this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    const newIndex = this.deck.indexOf(this.currentCard) + 1;
+    this.currentCard = this.deck[newIndex];
   }
 }
 
